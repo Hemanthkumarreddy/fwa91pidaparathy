fb3636e5f1a41bb902aa7124f0103e5d2a6a54cd
Hemanthkumarreddy <69857171+Hemanthkumarreddy@users.noreply.github.com>
2021-10-27 17:43:52 -0500

    new endpoints

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
-var indexRouter = require('./routes/index');
+//var indexRouter = require('./routes/index');
 //var usersRouter = require('./routes/users');
 var mydataRouter = require('./routes/mydata');
 
@@ -15,12 +15,12 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(logger('dev'));
-app.use(express.json('/mydata'));
+app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
-app.use('/', indexRouter);
+//app.use('/', indexRouter);
 //app.use('/users', usersRouter);
 app.use('/mydata', mydataRouter); 
 
