6ee948a22d8914763fb2143a0210595a3839970a
Hemanthkumarreddy <69857171+Hemanthkumarreddy@users.noreply.github.com>
2021-10-27 20:32:16 -0500

    Added bonus file

---
+++ routes/computation.js
@@ -0,0 +1,30 @@
+const express = require('express')
+const router = express.Router()
+
+router.get('/',(req,res,next) =>{
+    console.log(req.query)
+    
+        
+    if(Object.keys(req.query).length === 0){
+        
+        console.log("entered")
+        let power = Math.random()
+        res.render('computation', {value : `round applied to ${rand} is ${Math.pow(power)}`})
+    }
+    else
+    for(let a in req.query)
+    {
+    {
+        
+        console.log(a)
+    res.render('computation', {value : `round applied to ${req.query[a]} is ${Math.pow(req.query[a])}`})
+    }
+}
+
+   
+
+})
+
+
+
+module.exports = router
