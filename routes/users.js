36e60b627c215b3047c2cb4d7593d597efbce61e
Hemanthkumarreddy <69857171+Hemanthkumarreddy@users.noreply.github.com>
2021-10-27 20:04:56 -0500

    and more changes

--- routes/users.js
+++ routes/users.js
@@ -6,7 +6,7 @@ var numb = 0;
 router.get('/', function(req, res, next) {
   res.send('respond with a resource');
   numb = numb + 1;
-  res.send(`User accesses are: ${numb}`)
+  res.send('User accesses are: ${numb}');
 });
 
 module.exports = router;
