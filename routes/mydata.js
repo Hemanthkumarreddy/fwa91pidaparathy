ceb82caf4f448dcbbd986050b87a0151933d8dd7
Hemanthkumarreddy <69857171+Hemanthkumarreddy@users.noreply.github.com>
2021-10-27 19:08:06 -0500

    new endpoints

--- routes/mydata.js
+++ routes/mydata.js
@@ -2,7 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 /* GET fun facts about me page. */
-router.get('/mydata', function(req, res, next) {
+router.get('/', function(req, res, next) {
   res.render('mydata', { title: 'Hemanth Kumar Reddy Pidaparathy' });
 });
 
